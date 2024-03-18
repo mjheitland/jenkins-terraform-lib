6260fbc466fb73051c5b8b5ba8eee5ec756d6830
Michael Heitland <heitlm@amazon.de>
2024-03-18 09:49:20 +0100

    Update tfTest.groovy

--- vars/tfTest.groovy
+++ vars/tfTest.groovy
@@ -7,7 +7,7 @@ def call(Map config = [:]) {
     sh '''#!/usr/bin/env bash
         set -xeo pipefail
         go version
-        go env -w GOPROXY=https://proxy.golang.org,direct
+        go env -w GOPROXY=direct # https://proxy.golang.org,direct
         go get ./...
         go mod tidy
         go test -timeout $TF_TEST_TIMEOUT -v ./test/integration | tee $WORKSPACE/.artifacts/test-results.txt
