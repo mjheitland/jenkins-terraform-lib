846bb41684cca1d8a60c3f053f538e56d7c0b563
Michael Heitland <heitlm@amazon.de>
2024-03-24 10:05:18 +0100

    Update checkoutTags.groovy

--- vars/checkoutTags.groovy
+++ vars/checkoutTags.groovy
@@ -3,8 +3,10 @@ def call(Map config = [:]) {
     deleteDir()
 
     // checkout the branch that was triggering us
-    checkout scm
-
+    git branch: 'main'
+        credentialsID: 'jenkins_repo_access'
+        url: 'git@github.com:mjheitland/terraform-aws-example.git'
+    
     sh '''#!/usr/bin/env bash
         set -xeo pipefail
         git pull origin $BRANCH_NAME --tags
